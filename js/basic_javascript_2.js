c7f2f9e94de78bd4ea25c0bff9ae7c836e40dd84
ChristopherCordsen <c-cordsen@hotmail.com>
2019-10-15 01:38:42 -0700

    Commit Javascript to Master
    Page 114NOT
    Basic HTML5
    and fixed BasicHTML3

--- js/basic_javascript_2.js
+++ js/basic_javascript_2.js
@@ -1,1 +1,11 @@
-alert("Wake UP and smell the roses!");
+function ClickingFunctions() {
+    var String = "Blue Feathers are Magical!";
+    var result = String.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
+}
+
+function myFunction()   {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
---
+++ js/basic_javascript_5.js
@@ -0,0 +1,31 @@
+document.write("<h1>stuff</h1><p>I can do this all <strong>DAY!</strong></p><h2>I HATE MY CURRENT JOB!!!!!</h2>");
+
+document.write(2E310);
+
+document.write(10 < 2);
+
+console.log(2 + 2);
+
+document.write("10" + 5);
+
+document.write(10 == 10);
+
+document.write(3 == 3);
+
+document.write(4 == 9)
+
+x = 90;
+y = "90";
+document.write(x == y);
+
+a = "god";
+b = "dog";
+document.write(a === b);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(w > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
