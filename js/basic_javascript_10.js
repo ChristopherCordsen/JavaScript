0eb703220c4668d6b88dfbcf9afe5b13ec15c72b
ChristopherCordsen <c-cordsen@hotmail.com>
2019-10-21 17:53:42 -0700

    Commiting to Master
    Commiting all courses to master

---
+++ js/basic_javascript_10.js
@@ -0,0 +1,54 @@
+function count_To_Ten() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+}
+
+function lengthProperty() {
+    var str = "Yuppers Yup Yoooo";
+    var n = str.length;
+    document.getElementById("Length").innerHTML = n;
+}
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function cat_pics() {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "pooping";
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " +
+        Cat_Picture[3] + ".";
+}
+
+function constant_function() {
+    const Beebuts = {type:"Beebut", brand:"Fender", color:"black"};
+    Beebuts.color = "blue";
+    Beebuts.price = "$150.00";
+    document.getElementById("Constant").innerHTML = "The cost of the " +
+    Beebuts.type + " was " + Beebuts.price;
+}
+
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
---
+++ js/x.js
@@ -0,0 +1,12 @@
+var x = 82;
+document.write(x);
+{
+    let x = 33;
+    document.write("<br>" + x);
+}
+var x = 82;
+document.write(x);
+{
+    var x = 33;
+    documwnt.write("<br>" + x);
+}
